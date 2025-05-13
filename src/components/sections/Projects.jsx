5286a531158461fdb07a5ba31467b68195bb92eb
Subhadeep Paul <subhapaul637@gmail.com>
2025-05-13 16:07:53 +0530

    Update Projects.jsx

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -53,6 +53,44 @@ export const Projects = () => {
                 </a>
               </div>
             </div>
+             <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+             "
+             >
+              <h3 className="text-xl font-bold mb-2">Simplyfy</h3>
+              <p className="text-gray-400 mb-4">
+              A online  platform built with MERN stack.Here you can chat or video call with different users and share your thoughts with people with differnt languages .
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Express", "MongoDB", "TailwindCSS", "daisyui", "Stream", "Tanstack","Zustand","axios"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://simplyfy-video-chat.onrender.com"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
            <div
               className="
               glass p-6 rounded-xl border border-white/10 
